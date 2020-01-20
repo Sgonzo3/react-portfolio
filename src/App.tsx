7af16bd5c55c7c05dff102af06c8db0cb6cef722
Santos Gonzalez <sgonzo3@gmail.com>
2020-01-20 10:22:10 -0500

    feat: navbar and links to sections

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,29 @@ const App: React.FC = () => {
       <header className="App-header">
         <h1>Santos Gonzalez</h1>
       </header>
+      <nav className="nav">
+        <ul className="nav-links">
+          <li><a href="#featured-projects">Featured</a></li>
+          <li><a href="#events">Events</a></li>
+          <li><a href="#other-projects">Misc.</a></li>
+          <li><a href="#about-me">About Me</a></li>
+        </ul>
+      </nav>
       <main>
-        <section className="featured-projects">
+        <section id="featured-projects" className="featured-projects">
           <h2>Featured Projects</h2>
         </section>
-        <section className="events">
+        <section id="events" className="events">
           <h2>Events</h2>
         </section>
-        <section className="other-projects">
+        <section id="other-projects" className="other-projects">
           <h2>Misc. Projects</h2>
         </section>
-        <section className="about-me">
+        <section id="about-me" className="about-me">
           <h2>About Me</h2>
         </section>
       </main>
-      <footer>
+      <footer className="footer">
         <h2>Around The Web</h2>
         <ul className="profile-links">
           <li><a href="https://www.freecodecamp.org/sgonzo3">freeCodeCamp</a></li>
