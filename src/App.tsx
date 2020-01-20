428530c73996cdd0150ff7aebf9764eababc0b1c
Santos Gonzalez <sgonzo3@gmail.com>
2020-01-19 22:02:12 -0500

    feat: section headers

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Santos Gonzalez</h1>
       </header>
+      <main>
+        <section className="featured-projects">
+          <h2>Featured Projects</h2>
+        </section>
+        <section className="events">
+          <h2>Events</h2>
+        </section>
+        <section className="other-projects">
+          <h2>Misc. Projects</h2>
+        </section>
+        <section className="about-me">
+          <h2>About Me</h2>
+        </section>
+      </main>
+      <footer>
+        <h2>Around The Web</h2>
+        <ul className="profile-links">
+          <li><a href="https://www.freecodecamp.org/sgonzo3">freeCodeCamp</a></li>
+          <li><a href="https://github.com/Sgonzo3">Github</a></li>
+          <li><a href="https://www.linkedin.com/in/sgonzo3/">LinkedIn</a></li>
+        </ul>
+        <p>Made by <a href="https://sgonzo3.github.io/">Santos Gonzalez</a></p>
+        <p>All Rights Reserved</p>
+      </footer>
     </div>
   );
 }
